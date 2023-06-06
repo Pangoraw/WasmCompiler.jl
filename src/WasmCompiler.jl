8ec5a1b2af3842c492151997d977062927cc8fea
Paul Berg <naydex.mc+github@gmail.com>
2023-06-06 12:41:59 +0200

    styled print

--- src/WasmCompiler.jl
+++ src/WasmCompiler.jl
@@ -1,6 +1,7 @@
 module WasmCompiler
 
 include("./wasm.jl")
+include("./wat.jl")
 include("./optimize.jl")
 
 end # module WasmCompiler
---
+++ src/wat.jl
@@ -0,0 +1,206 @@
+const INDENT_S = "  "
+
+function _printwasm(io::IO, mod::WModule)
+    println(io, "(")
+    _printkw(io, "module")
+    indent = 2
+
+    for type in mod.types
+        error("cannot export types $type")
+        # print(io, INDENT_S^indent, "(type)")
+        # println(io)
+    end
+
+    ctx = IOContext(io, :indent => indent)
+    for func in mod.funcs
+        _printwasm(ctx, func)
+        println(io)
+        println(io)
+    end
+
+    if !isnothing(mod.start)
+        print(io, INDENT_S^indent, "(")
+        _printkw(io, "start")
+        print(io, " ")
+        func = mod.funcs[mod.start]
+        if !isnothing(func.name)
+            print(io, "\$", func.name)
+        else
+            print(io, mod.start - 1)
+        end
+        println(io, ")")
+    end
+
+    for imp in mod.imports
+        print(io, INDENT_S^indent, "(")
+        _printkw(io, "import")
+        print(io, " \"$(imp.module_name)\" \"$(imp.name)\"")
+        if imp isa FuncImport
+            print(io, "(")
+            _printkw(io, "func")
+            print(io, " \$", imp.id)
+            _printwasm(io, imp.fntype)
+            print(io, ")")
+        else
+            error("cannot handle import $imp")
+        end
+        println(io, ")")
+    end
+
+    println(io)
+    for exp in mod.exports
+        print(io, INDENT_S^indent, "(")
+        _printkw(io, "export")
+        print(io, " ")
+        if exp isa FuncExport
+            func = mod.funcs[exp.func]
+            print(io, "\"$(exp.name)\" ", "(")
+            _printkw(io, "func")
+            print(io, " ")
+            if !isnothing(func.name)
+                print(io, '$', func.name)
+            else
+                print(io, exp.func - 1)
+            end
+            print(io, ")")
+        else
+            error("cannot handle export $exp")
+        end
+        print(io, ")")
+    end
+    print(io, ")")
+end
+
+function _printwasm(io::IO, fntype::FuncType)
+    compact = get(io, :compact, true)
+    if compact
+        print(io, "("); _printkw(io, "param")
+        for param in fntype.params
+            print(io, " ", param)
+        end
+        print(io, ") ("); _printkw(io, "result")
+        for result in fntype.results
+            print(io, " ", result)
+        end
+        print(io, ")")
+    else
+        for param in fntype.params
+            print(io, "(")
+            _printkw(io, "param")
+            print(io, " ", param, ") ")
+        end
+        for result in fntype.results
+            print(io, "(")
+            _printkw(io, "result")
+            print(io, " ", result, ") ")
+        end
+    end
+    println(io)
+end
+
+_printkw(io::IO, kw) = get(io, :color, false) ?  printstyled(io, kw; color=:red) : print(io, kw)
+function _printinst(io::IO, s)
+    print(io, INDENT_S^get(io, :indent, 2))
+    get(io, :color, false) ?
+        printstyled(io, s; color=:magenta) : print(io, s)
+end
+
+_printwasm(io::IO, ::return_) = _printinst(io, "return")
+
+_printwasm(io::IO, ::i32_lt_s) = _printinst(io, "i32.lt_s")
+_printwasm(io::IO, ::i32_lt_u) = _printinst(io, "i32.lt_u")
+_printwasm(io::IO, ::i64_lt_s) = _printinst(io, "i64.lt_s")
+_printwasm(io::IO, ::i64_lt_u) = _printinst(io, "i64.lt_u")
+_printwasm(io::IO, ::i32_le_s) = _printinst(io, "i32.le_s")
+_printwasm(io::IO, ::i32_le_u) = _printinst(io, "i32.le_u")
+_printwasm(io::IO, ::i64_le_s) = _printinst(io, "i64.le_s")
+_printwasm(io::IO, ::i64_le_u) = _printinst(io, "i64.le_u")
+
+_printwasm(io::IO, ::i64_extend_i32_s) = _printinst(io, "i64.extend_i32_s")
+_printwasm(io::IO, ::i64_extend_i32_u) = _printinst(io, "i64.extend_i32_u")
+
+function _printwasm(io::IO, inst::Inst)
+    name = string(nameof(typeof(inst)))
+    _printinst(io, replace(name, "_" => "."))
+    for f in fieldnames(typeof(inst))
+        print(io, " ", getfield(inst, f))
+    end
+end
+
+function _printwasm(io::IO, f::Func)
+    indent = get(io, :indent, 0)::Int
+    print(io, INDENT_S^indent, "(")
+    _printkw(io, "func")
+    print(io, " ")
+    if !isnothing(f.name)
+        print(io, '$', f.name, " ")
+    end
+    _printwasm(io, f.fntype)
+    compact = get(io, :compact, true)
+    if compact
+        print(io, INDENT_S^(indent+2), "(")
+        _printkw(io, "local")
+        print(io, " ")
+        for loc in Iterators.drop(f.locals, length(f.fntype.params))
+            print(io, " ", loc)
+        end
+        println(io, ")")
+    else
+        for loc in Iterators.drop(f.locals, length(f.fntype.params))
+            print(io, INDENT_S^(indent+2), "(")
+            _printkw(io, "local")
+            println(io, ", loc, ")")
+        end
+    end
+    ctx = IOContext(io, :indent => indent + 2)
+    _printwasm(ctx, f.inst)
+    print(io, INDENT_S^indent, ")")
+end
+
+function _printwasm(io::IO, loop::Loop)
+    indent = get(io, :indent, 2)
+    print(io, INDENT_S^indent)
+    _printkw(io, "loop")
+    print(io, " ")
+    _printwasm(io, loop.fntype)
+    ctx = IOContext(io, :indent => indent + 2)
+    _printwasm(ctx, loop.inst)
+    print(io, INDENT_S^indent)
+    _printkw(io, "end")
+end
+
+function _printwasm(io::IO, i::If)
+    indent = get(io, :indent, 2)
+    print(io, INDENT_S^indent)
+    _printkw(io, "if")
+    print(io, " ")
+    _printwasm(io, i.fntype)
+    ctx = IOContext(io, :indent => indent + 2)
+    _printwasm(ctx, i.trueinst)
+    print(io, INDENT_S^indent)
+    _printkw(io, "else")
+    println(io)
+    _printwasm(ctx, i.falseinst)
+    print(io, INDENT_S^indent)
+    _printkw(io, "end")
+end
+
+function _printwasm(io::IO, expr::Vector{Inst})
+    ctx = IOContext(io, :indent => get(io, :indent, 0))
+    for inst in expr
+        _printwasm(ctx, inst)
+        println(io)
+    end
+end
+
+function _printwasm(io::IO, b::Block)
+    indent = get(io, :indent, 2)
+    print(io, INDENT_S^indent)
+    _printkw(io, "block")
+    print(io, " ")
+    _printwasm(io, b.fntype)
+    ctx = IOContext(io, :indent => indent + 2)
+    _printwasm(ctx, b.inst)
+    print(io, INDENT_S^indent)
+    _printkw(io, "end")
+end
