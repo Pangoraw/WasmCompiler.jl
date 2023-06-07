f9672f47214754003ff68ce5abca7e515d43afe8
Paul Berg <naydex.mc+github@gmail.com>
2023-06-07 10:23:04 +0200

    @code_wasm

--- src/WasmCompiler.jl
+++ src/WasmCompiler.jl
@@ -4,4 +4,25 @@ include("./wasm.jl")
 include("./wat.jl")
 include("./optimize.jl")
 
+struct Wat
+    obj
+end
+
+Base.show(io::IO, wat::Wat) = WasmCompiler._printwasm(io, wat.obj)
+
+macro code_wasm(ex)
+    @assert Meta.isexpr(ex, :call)
+
+    args = esc(Expr(:tuple, ex.args[begin+1:end]...))
+    f = esc(ex.args[1])
+
+    quote
+        types = Tuple{map(Core.Typeof, $(args))...}
+        func = WasmCompiler.emit_func($f, types)
+        Wat(func)
+    end
+end
+
+export @code_wasm
+
 end # module WasmCompiler
